6a6ed221afb8ac9913a824bb76d0c3f085222bb5
Matt Calthrop <mcalthrop@gmail.com>
2017-03-28 11:57:19 +0100

    Minor comment update

--- controllers/games-controller.js
+++ controllers/games-controller.js
@@ -5,7 +5,7 @@ function indexGames(req, res) {
   Game.find({}, function (err, games) {
     if (err) {
       console.log('Could not get list of games:', err);
-      // NOTE: when an error occurs, always create an object that looks like this:
+      // NOTE: when an error occurs, always return a JSON object that looks like this:
       //   { message: 'description of error' }
       // This will enable the client (UI) to display an error message.
       res.status(500).json({ message: 'Could not get list of games' });
